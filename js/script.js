20888caf3c23ee67baf024a9d0991d7cf5ada3ba
AndreiApanel <aposha84@gmail.com>
2023-05-01 18:16:19 +0200

    script mod_second

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-const numberOfFilms = promt("Сколько фильмов вы уже просмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    lastFilm: yourOpinies,
+  },
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const lastFilm = prompt("Один из последних просмотренных фильмов?");
+const yourOpinies = prompt("На сколько оцените его?");
