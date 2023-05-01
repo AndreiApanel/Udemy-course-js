ffc53dc1c5c47c11c71234d1cabfffab4bee6cb1
AndreiApanel <aposha84@gmail.com>
2023-05-01 18:33:06 +0200

    fixed script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
 const personalMovieDB = {
   count: numberOfFilms,
-  movies: {
-    lastFilm: yourOpinies,
-  },
+  movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
-const lastFilm = prompt("Один из последних просмотренных фильмов?");
-const yourOpinies = prompt("На сколько оцените его?");
+
+const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const yourOpinies = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[lastFilm] = yourOpinies;
+
+console.log(personalMovieDB);
