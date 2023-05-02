c356f34db82b28fbebe37abcbbf004d3efa9e728
AndreiApanel <aposha84@gmail.com>
2023-05-02 13:50:46 +0200

    A five tasks

--- js/script.js
+++ js/script.js
@@ -22,3 +22,28 @@ for (let i = 1; i < length; i++) {
 }
 
 console.log(result);
+first: for (let i = 0; i < 3; i++) {
+  console.log(`first level: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`second level: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first;
+      console.log(`third level: ${k}`);
+    }
+  }
+}
+let i = 2;
+do {
+  i++;
+  if (i % 2 === 0) {
+    continue;
+  } else {
+    console.log(i);
+  }
+} while (i <= 16);
+
+const arrayOfNumbers = [];
+for (let i = 5; i <= 10; i++) {
+  arrayOfNumbers.push(i);
+  console.log(arrayOfNumbers);
+}
