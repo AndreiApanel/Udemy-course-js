49eb056a4a2857910b3d7df10f5e93c9b6c10df0
AndreiApanel <aposha84@gmail.com>
2023-05-02 15:07:41 +0200

    Task js ***

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -7,18 +7,18 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const yourOpinies = prompt("На сколько оцените его?", "");
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const yourOpinies = prompt('На сколько оцените его?', '');
 personalMovieDB.movies[lastFilm] = yourOpinies;
 
 console.log(personalMovieDB);
-let result = "";
+let result = '';
 const length = 7;
 for (let i = 1; i < length; i++) {
   for (let j = 0; j < i; j++) {
-    result += "*";
+    result += '*';
   }
-  result += "\n";
+  result += '\n';
 }
 
 console.log(result);
@@ -54,7 +54,7 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log(result);
 
-const data = [5, 10, "Shopping", 20, "Homework"];
+const data = [5, 10, 'Shopping', 20, 'Homework'];
 const result = [];
 for (let i = data.length - 1; i >= 0; i--) {
   result.push(data[i]);
