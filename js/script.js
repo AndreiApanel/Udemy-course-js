9c303d66b2c87e4e8283bdccf14a1444b0d63489
AndreiApanel <aposha84@gmail.com>
2023-05-28 19:41:31 +0200

    Task decision 13*

--- js/script.js
+++ js/script.js
@@ -483,3 +483,45 @@
 //     });
 //   },
 // };
+// ------------------------------------------------------------------------
+
+// const shoppingMallData = {
+//   shops: [
+//     {
+//       width: 10,
+//       length: 5,
+//     },
+//     {
+//       width: 15,
+//       length: 7,
+//     },
+//     {
+//       width: 20,
+//       length: 5,
+//     },
+//     {
+//       width: 8,
+//       length: 10,
+//     },
+//   ],
+//   height: 5,
+//   moneyPer1m3: 30,
+//   budget: 50000,
+// };
+
+// function isBudgetEnough(data) {
+//   let placeholder = 0;
+//   let volume = 0;
+//   data.shops.forEach((el) => {
+//     placeholder += el.width * el.length;
+//   });
+//   volume = placeholder * data.height;
+//   if (data.budget - volume * data.moneyPer1m3 >= 0) {
+//     return 'Бюджета достаточно';
+//   } else {
+//     return 'Бюджета недостаточно';
+//   }
+// }
+
+// console.log(isBudgetEnough(shoppingMallData));
+// ------------------------------------------------------------------------
