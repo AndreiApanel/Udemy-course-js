196cdd61feebe34ed32b3395fb87dca9b0f648bf
AndreiApanel <aposha84@gmail.com>
2023-07-31 20:01:14 +0200

    ClassList Done

--- js/script.js
+++ js/script.js
@@ -780,11 +780,23 @@
 // }
 
 // console.log(deepCount([1, 2, [3, 4, [5]]]));
-const btns = document.querySelectorAll('button');
-btns[0].addEventListener('click', () => {
-  if (!btns[1].classList.contains('red')) {
-    btns[1].classList.add('red');
-  } else {
-    btns[1].classList.remove('red');
-  }
-});
+// const btns = document.querySelectorAll('button');
+// wrapper = document.querySelector('.btn-block');
+// btns[0].addEventListener('click', () => {
+// if (!btns[1].classList.contains('red')) {
+//   btns[1].classList.add('red');
+// } else {
+//   btns[1].classList.remove('red');
+// }
+//   btns[1].classList.toggle('red');
+// });
+
+// wrapper.addEventListener('click', (e) => {
+//   if (e.target && e.target.matches('button.red')) {
+//     console.log('hello');
+//   }
+// });
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
