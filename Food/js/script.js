25743b3a80caf934cb3457595ae6036c8c869161
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2023-12-04 20:27:05 +0100

    fix: add getZero function

--- Food/js/script.js
+++ Food/js/script.js
@@ -53,6 +53,14 @@ window.addEventListener('DOMContentLoaded', () => {
       seconds: seconds,
     };
   }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
   function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
       days = timer.querySelector('#days'),
@@ -61,13 +69,15 @@ window.addEventListener('DOMContentLoaded', () => {
       seconds = timer.querySelector('#seconds');
     timeInterval = setInterval(updateClock, 1000);
 
+    updateClock();
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
 
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZerot(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
