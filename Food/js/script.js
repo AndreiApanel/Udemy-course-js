a8dd7b8c825c4b7bce054a6ebde0a51a4062bf19
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2023-12-09 22:37:57 +0100

    add: function open-close in modal window

--- Food/js/script.js
+++ Food/js/script.js
@@ -93,4 +93,20 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
   setClock('.timer', deadline);
+
+  // modal
+
+  const modalTrigger = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+  modalTrigger.addEventListener('click', () => {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+  });
+  modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  });
 });
