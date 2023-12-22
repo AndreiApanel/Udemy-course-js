aefa975638bc6977738a548c203758ae15c2460c
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2023-12-22 20:41:21 +0100

    add: 4 rules of add a method this

--- js/script.js
+++ js/script.js
@@ -827,22 +827,50 @@
 // #76
 // This
 
-function showThis(a, b) {
-	console.log(this);
-	function sum() {
-		console.log(this);
-		return this.a + this.b;
-	}
-	console.log(sum());
-}
-showThis(4, 5);
-const obj = {
-	a: 20,
-	b: 15,
-	sum: function () {
-		console.log(this);
-	},
-};
-obj.sum();
+// function showThis(a, b) {
+// 	console.log(this);
+// 	function sum() {
+// 		console.log(this);
+// 		return this.a + this.b;
+// 	}
+// 	console.log(sum());
+// }
+// showThis(4, 5);
+// const obj = {
+// 	a: 20,
+// 	b: 15,
+// 	sum: function () {
+// 		console.log(this);
+// 	},
+// };
+// obj.sum();
+// function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// 	this.hello = function () {
+// 		console.log(`Hello ${this.name}`);
+// 	};
+// }
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+// 	console.log(this);
+// 	console.log(this.name + surname);
+// }
+// const user = {
+// 	name: 'John',
+// };
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+// function count(num) {
+// 	return this * num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
 // 1) Обычная функция: this = window, но если use strict = undefined;
-// 2)
+// 2) Контекст у методов обьекта - сам обьект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
