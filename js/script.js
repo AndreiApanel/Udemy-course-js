3d4eb61c916c97efab5d48c6b69ef02c2bec500f
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-02-18 14:22:01 +0100

    #77 Finished

--- js/script.js
+++ js/script.js
@@ -894,6 +894,36 @@
 // console.log(double(4));
 
 // в обработчике события стрелочная функция с this возвращает undefined (контекст - window)
---------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
 
 // #77 Классы (ES6)
+
+// class Rectangle {
+// 	constructor(height, width) {
+// 		this.height = height;
+// 		this.width = width;
+// 	}
+// 	calcArea() {
+// 		return this.height * this.width;
+// 	}
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+// 	constructor(height, width, text, bgColor) {
+// 		super(height, width);
+// 		this.text = text;
+// 		this.bgColor = bgColor;
+// 	}
+// 	showMyProps() {
+// 		console.log(`text: ${this.text},color: ${this.bgColor}`);
+// 	}
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'Hello Punk', 'red');
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(5, 10);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
