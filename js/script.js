15eeddf62e314b8e5ed97923d386879ca9f9fbd0
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-03-13 19:36:14 +0100

    # 86 Promise (ES6). all/race

--- js/script.js
+++ js/script.js
@@ -988,33 +988,51 @@
 // 	// readyState
 // });
 //# 86 Promise (ES6)
-console.log('Data request...');
-const req  = new Promise(function (resolve, reject) {
-	setTimeout(() => {
-		console.log('Preparation of data');
-		const product = {
-			name: 'TV',
-			price:'1000 zlot',
-		}
-resolve(product);
-	},2000);
-});
+// console.log('Data request...');
+// const req  = new Promise(function (resolve, reject) {
+// 	setTimeout(() => {
+// 		console.log('Preparation of data');
+// 		const product = {
+// 			name: 'TV',
+// 			price:'1000 zlot',
+// 		}
+// resolve(product);
+// 	},2000);
+// });
 
-req.then((product)=>{
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-		product.status = 'order';
-		resolve(product);
-		},2000);
-	});
-}).then(data =>{
-	data.modify = true;
-	return data;
+// req.then((product)=>{
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 		product.status = 'order';
+// 		resolve(product);
+// 		},2000);
+// 	});
+// }).then(data =>{
+// 	data.modify = true;
+// 	return data;
 
-}).then (data =>{
-	console.log(data);
-})
+// }).then (data =>{
+// 	console.log(data);
+// }).catch(()=>{
+// 	console.error('Error');
+// }).finally(()=>{
+// 	console.log('Finally');
+// })
 
+// const test = time => {
+// 	return new Promise (resolve => {
+// 		setTimeout(() => resolve(), time);
+// 	});
+// };
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all ([test(1000), test(2000)]).then(()=> {
+// 	console.log(('all'));
+// });
+// Promise.race ([test(1000), test(2000)]).then(()=> {
+// 	console.log(('all'));
+// });
 	
 
 
