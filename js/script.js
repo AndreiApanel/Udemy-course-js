fe02e44a51539c5406204da39ed004a469cd4771
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-03-19 07:07:22 +0100

    # 21 Task. Part one complited

--- js/script.js
+++ js/script.js
@@ -1132,3 +1132,21 @@
 // 	return arr.every(item =>item.id || item.id === 0 ? true : false)
 // }
 // console.log(checkFilms(tranformedArray));
+
+// # 21 Task
+
+const funds = [
+	{amount: -1400},
+	{amount: 2400},
+	{amount: -1000},
+	{amount: 500},
+	{amount: 10400},
+	{amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data) => {
+	return data.filter(item => item.amount >= 0).map((e) =>e.amount).reduce((sum, item) => sum + item);
+
+}
+
+console.log(getPositiveIncomeAmount(funds));
