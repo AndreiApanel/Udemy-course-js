5677d23c0c1fe444f5da4de335fef5d4d255eda9
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-04-13 12:00:10 +0200

    # 95 Using localStorage

--- js/script.js
+++ js/script.js
@@ -1160,3 +1160,51 @@
 // }
 
 // console.log(getTotalIncomeAmount(funds));
+
+// #95 localStorage
+
+// localStorage.setItem('number', 5);
+//  localStorage.removeItem('number');
+//  localStorage.clear();
+//  console.log(localStorage('number'));
+const checkbox = document.querySelector('#checkbox'),
+	form = document.querySelector('form'),
+	change = document.querySelector('#color');
+
+	if(localStorage.getItem('bg') === 'changed')
+	{
+	 
+	 form.style.backgroundColor = 'red'
+	}
+
+if(localStorage.getItem('isChecked')) {
+checkbox.checked = true;
+}
+
+
+
+change.addEventListener('click', () => {
+	if(localStorage.getItem('bg') === 'changed')
+	 {
+		localStorage.removeItem('bg');
+		form.style.backgroundColor = 'white'
+	}else{
+		localStorage.setItem('bg', 'changed');
+		form.style.backgroundColor = 'red'
+	}
+}
+);
+
+checkbox.addEventListener('change', () => {
+	localStorage.setItem('isChecked', true);
+})
+
+
+const persone = {
+	name: 'Alexander',
+	age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex',serializedPersone);
+console.log(JSON.parse(localStorage.getItem('alex')));
