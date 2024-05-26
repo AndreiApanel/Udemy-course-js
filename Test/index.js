d1fa574aef983e920195a01fbbd80ec4ebcc3e09
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-05-26 18:49:54 +0200

    Test #102 Webpack

---
+++ Test/index.js
@@ -0,0 +1,8 @@
+// #102 Webpack
+const myModule = require('./main');
+const myModuleInstance = new myModule();
+myModuleInstance.hello();
+myModuleInstance.goodbye();
+// gulp - планировщик задач,
+// webpack - сборщик модулей,
+// их можно комбинировать
