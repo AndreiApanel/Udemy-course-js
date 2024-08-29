4a3fa4ce0ff1437eb1211f0f8cdd4a1f8556dac9
Andrei <110493268+AndreiApanel@users.noreply.github.com>
2024-08-29 19:54:39 +0200

    Finally test

--- js/src/test.js
+++ js/src/test.js
@@ -1461,3 +1461,69 @@
 // render
 // () => {}
 // microtasks: then/catch/finally/await
+// const msg = 'My number +12345678, name: Oleg';
+
+// function transformMsg(str) {
+// 	// Пропущенная часть
+// 	let a = str.replace(/\+\d{8}/, '*****');
+// 	let b = a.replace(/\w{4}:\s\w{1,}/, 'hidden');
+// 	return b;
+// }
+
+// console.log(transformMsg(msg));
+// if (0) {
+// 	console.log('first')
+// } else if (NaN) {
+// 	console.log('second');
+// } else if (' ') {
+// 	console.log('third');
+// } else if (null) {
+// 	console.log('fourth');
+// }.
+// const promise = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 			resolve('foo');
+// 	}, 1000);
+// 	setTimeout(() => {
+// 			reject('bar');
+// 	}, 900);
+// });
+
+// promise.then((value) => {
+// 	console.log(value);
+// }).catch((e) => console.log(e))
+// console.log(0 && 1)
+// combineUrls('https', 'mysite.com');
+ 
+// const combineUrls = (protocol, domain) => {
+//     return `${protocol}://${domain}`;
+// }
+// const promisify = (item, delay) =>
+// 	new Promise(resolve => setTimeout(() => resolve(item), delay));
+
+// const a = () => promisify('a', 100);
+// const b = () => promisify('b', 5000);
+// const c = () => promisify('c', 3000);
+
+// async function one() {
+// 	const promises = [a(), b(), c()];
+// 	const [outpu1, outpu2, outpu3] = await Promise.all(promises);
+// 	return `one is done: ${outpu1} ${outpu2} ${outpu3}`;
+// }
+
+// async function two() {
+// 	const promises = [a(), b(), c()];
+// 	const outpu1 = await Promise.race(promises);
+// 	return `two is done: ${outpu1}`;
+// }
+
+// async function three() {
+// 	const outpu1 = await a();
+// 	const outpu2 = await b();
+// 	const outpu3 = await c();
+// 	return `three is done: ${outpu1} ${outpu2} ${outpu3}`
+// }
+
+// one().then(console.log);
+// two().then(console.log);
+// three().then(console.log);
