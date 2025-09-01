a83350a90dc148d4a183e940b182eedb5543a539
Andrei Apanel <aposha84@gmail.com>
2025-09-01 12:49:47 +0200

    196. Connecting React and Redux using connect. Used the class.

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,22 +1,41 @@
+import {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 // import {bindActionCreators} from 'redux';
-const Counter = ({counter, inc, dec, rnd}) => {
-  return (
-    <div className="jumbotron">
-      <h1>{counter}</h1>
-      <button className="btn btn-primary" onClick={inc}>
-        Increment
-      </button>
-      <button className="btn btn-primary" onClick={dec}>
-        Decrement
-      </button>
-      <button className="btn btn-primary" onClick={rnd}>
-        Random
-      </button>
-    </div>
-  );
-};
+class Counter extends Component {
+  render() {
+    const {counter, inc, dec, rnd} = this.props;
+    return (
+      <div className="jumbotron">
+        <h1>{counter}</h1>
+        <button className="btn btn-primary" onClick={inc}>
+          Increment
+        </button>
+        <button className="btn btn-primary" onClick={dec}>
+          Decrement
+        </button>
+        <button className="btn btn-primary" onClick={rnd}>
+          Random
+        </button>
+      </div>
+    );
+  }
+}
+
+//     <div className="jumbotron">
+//       <h1>{counter}</h1>
+//       <button className="btn btn-primary" onClick={inc}>
+//         Increment
+//       </button>
+//       <button className="btn btn-primary" onClick={dec}>
+//         Decrement
+//       </button>
+//       <button className="btn btn-primary" onClick={rnd}>
+//         Random
+//       </button>
+//     </div>
+//   );
+// };
 
 const mapStateToProps = state => {
   return {
