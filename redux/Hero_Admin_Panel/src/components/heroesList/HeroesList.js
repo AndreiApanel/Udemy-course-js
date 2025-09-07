d86da9056715476cfbae9ae99d962752bc6a6cfb
Andrei Apanel <aposha84@gmail.com>
2025-09-07 17:54:17 +0200

    199. The rule of the verb "action" and homework (mini-exam) fix. 1 Part is ok.

--- redux/Hero_Admin_Panel/src/components/heroesList/HeroesList.js
+++ redux/Hero_Admin_Panel/src/components/heroesList/HeroesList.js
@@ -1,9 +1,9 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
-import HeroesListItem from "../heroesListItem/HeroesListItem";
+import {heroesFetching, heroesFetched, heroesFetchingError, heroDeleted} from '../../actions';
+import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
 
 // Задача для этого компонента:
@@ -12,48 +12,46 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
+  const dispatch = useDispatch();
+  const {request} = useHttp();
 
-    useEffect(() => {
-        dispatch(heroesFetching());
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+  const heroes = useSelector(state => state.heroes);
+  const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
 
-        // eslint-disable-next-line
-    }, []);
+  useEffect(() => {
+    dispatch(heroesFetching());
+    request('http://localhost:3001/heroes')
+      .then(data => dispatch(heroesFetched(data)))
+      .catch(() => dispatch(heroesFetchingError()));
 
-    if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
-    } else if (heroesLoadingStatus === "error") {
-        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    // eslint-disable-next-line
+  }, []);
+
+  if (heroesLoadingStatus === 'loading') {
+    return <Spinner />;
+  } else if (heroesLoadingStatus === 'error') {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+  }
+
+  const renderHeroesList = arr => {
+    if (arr.length === 0) {
+      return <h5 className="text-center mt-5">Героев пока нет</h5>;
     }
 
-    const renderHeroesList = (arr) => {
-        if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
-        }
+    return arr.map(({id, ...props}) => {
+      return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)} />;
+    });
+  };
 
-        return arr.map(({id, ...props}) => {
-                return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)}/>
-        })
-    }
+  const onDelete = id => {
+    // Delete from server
+    request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+      .then(() => dispatch(heroDeleted(id)))
+      .catch(() => dispatch(heroesFetchingError()));
+  };
 
-        const onDelete = (id) => {
-            // Delete from server
-            request(`http://localhost:3001/heroes/${id}`, 'DELETE')
-                .then(() => dispatch(heroDeleted(id)))
-                .catch(() => dispatch(heroesFetchingError()));
-        }
-
-    const elements = renderHeroesList(heroes);
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
-}
+  const elements = renderHeroesList(heroes);
+  return <ul>{elements}</ul>;
+};
 
 export default HeroesList;
