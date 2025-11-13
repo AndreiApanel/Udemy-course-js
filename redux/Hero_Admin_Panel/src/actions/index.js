315c85311c8d584c29f2796f6a64d39713369f5f
Andrei Apanel <aposha84@gmail.com>
2025-11-13 22:43:36 +0100

    208. Redux Toolkit: createReducer()

--- redux/Hero_Admin_Panel/src/actions/index.js
+++ redux/Hero_Admin_Panel/src/actions/index.js
@@ -21,18 +21,20 @@ export const heroesFetching = createAction('HEROES_FETCHING');
 //   };
 // };
 export const heroesFetched = createAction('HEROES_FETCHED');
-export const heroesFetchingError = () => {
-  return {
-    type: 'HEROES_FETCHING_ERROR',
-  };
-};
+// export const heroesFetchingError = () => {
+//   return {
+//     type: 'HEROES_FETCHING_ERROR',
+//   };
+// };
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const activeFilterChanged = filter => {
-  return {
-    type: 'ACTIVE_FILTER_CHANGED',
-    payload: filter,
-  };
-};
+// export const activeFilterChanged = filter => {
+//   return {
+//     type: 'ACTIVE_FILTER_CHANGED',
+//     payload: filter,
+//   };
+// };
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
 // export const activeFilterChanged = filter => dispatch => {
 //   setTimeout(() => {
 //     dispatch({
@@ -41,19 +43,9 @@ export const activeFilterChanged = filter => {
 //     });
 //   }, 1000);
 // };
-export const heroCreated = hero => {
-  return {
-    type: 'HERO_CREATED',
-    payload: hero,
-  };
-};
+export const heroCreated = createAction('HERO_CREATED');
 
-export const heroDeleted = id => {
-  return {
-    type: 'HERO_DELETED',
-    payload: id,
-  };
-};
+export const heroDeleted = createAction('HERO_DELETED');
 
 export const fetchFilters = request => dispatch => {
   dispatch(filtersFetching());
@@ -62,21 +54,8 @@ export const fetchFilters = request => dispatch => {
     .catch(() => dispatch(filtersFetchingError()));
 };
 
-export const filtersFetching = () => {
-  return {
-    type: 'FILTERS_FETCHING',
-  };
-};
+export const filtersFetching = createAction('FILTERS_FETCHING');
 
-export const filtersFetched = filters => {
-  return {
-    type: 'FILTERS_FETCHED',
-    payload: filters,
-  };
-};
+export const filtersFetched = createAction('FILTERS_FETCHED');
 
-export const filtersFetchingError = () => {
-  return {
-    type: 'FILTERS_FETCHING_ERROR',
-  };
-};
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
