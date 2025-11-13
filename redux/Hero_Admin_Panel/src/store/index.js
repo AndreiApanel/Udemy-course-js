7bff91bbcb291e1371801bbf5b286a15a0fdf388
Andrei Apanel <aposha84@gmail.com>
2025-11-13 21:19:25 +0100

    203. Store enhancers

--- redux/Hero_Admin_Panel/src/store/index.js
+++ redux/Hero_Admin_Panel/src/store/index.js
@@ -1,13 +1,39 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {configureStore, combineReducers, compose} from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
 const rootReducer = combineReducers({
   heroes,
   filters,
 });
+
+const enhancer =
+  createStore =>
+  (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = action => {
+      if (typeof action === 'string') {
+        return oldDispatch({type: action});
+      }
+      return oldDispatch(action);
+    };
+    return store;
+  };
+
 const store = configureStore({
   reducer: rootReducer,
+  enhancers: getDefaultEnhancers => getDefaultEnhancers().concat(enhancer),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
+// const rootReducer = combineReducers({
+//   heroes,
+//   filters,
+// });
+// const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: process.env.NODE_ENV !== 'production',
+// });
